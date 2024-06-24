9cf4c50bdbf9b094f5717ad48c541026192c283c
ansarirayyan01 <ansarirayyan874@gmail.com>
2024-06-24 23:08:18 +0530

    done with the import and exporting of component

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -5,6 +5,13 @@ import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from './Button'
 import Input from './Input'
+import Signup from "./SignUp";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout"
+import RTE from "./RTE";
+import Select from "./Select"
 
 export {
     Header,
@@ -14,4 +21,11 @@ export {
     LogoutBtn,
     Button,
     Input,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    RTE,
+    Select,
 }
